95eabf1a95c27777db2cefa81776850e45c4013e
wonder <waxz@users.noreply.github.com>
2023-02-07 11:26:47 +0800

    Add files via upload

---
+++ pages/Plot.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
